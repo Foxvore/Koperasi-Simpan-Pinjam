b423660203699349cd86ace46d04363ddbd9776f
Foxvore <imjohani2005@gmail.com>
2022-08-12 16:36:13 +0700

    first commit

--- src/assets/local/script.js
+++ src/assets/local/script.js
@@ -145,6 +145,16 @@
   /**
    * Animation on scroll init
    */
-   AOS.init();
-
+   function aos_init() {
+    AOS.init({
+      duration: 800,
+      easing: 'slide',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+  
 })()
