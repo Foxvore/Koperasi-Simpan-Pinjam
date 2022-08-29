0becf1b0220aab53caeeb7442d67a2e1a7c3ff0f
Foxvore <imjohani2005@gmail.com>
2022-08-29 10:56:21 +0700

    Adding Back To Top Button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 		<router-view />
-    <!-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>  -->
+    <a href="#" class="back-to-top active d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> 
 	</div>
 </template>
 
