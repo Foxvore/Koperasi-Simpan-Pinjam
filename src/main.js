4454b9bfac961885cd3f8f690962718be2f12709
Foxvore <imjohani2005@gmail.com>
2022-09-12 09:16:59 +0700

    Add Sweetalert2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 import router from './router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(router).use(MotionPlugin).mount('#app');
+createApp(App)
+    .use(router)
+    .use(MotionPlugin)
+    .use(VueSweetalert2)
+    .mount('#app');
