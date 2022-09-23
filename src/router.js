de83b26aa7571f52ee4cb54c37f9a8e7c8aaee65
Foxvore <imjohani2005@gmail.com>
2022-09-23 10:02:37 +0700

    Add Admin Router

--- src/router.js
+++ src/router.js
@@ -7,6 +7,13 @@ import Profile from './components/Profile.vue'
 import Pegawai from './components/Pegawai.vue'
 import Anggota from './components/Anggota.vue'
 import Pengajuan from './components/Pengajuan.vue'
+import Dashboard from './components/Dashboard.vue'
+import A_Akun from './components/A-Akun.vue'
+import A_Pegawai from './components/A-Pegawai.vue'
+import A_Anggota from './components/A-Anggota.vue'
+import A_Simpanan from './components/A-Simpanan.vue'
+import A_Pinjaman from './components/A-Pinjaman.vue'
+import Api from './components/Api.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -54,6 +61,41 @@ const routes = [
         name: 'Pengajuan',
         component: Pengajuan,
         path: '/pengajuan'
+    },
+    {
+        name: 'Dashboard',
+        component: Dashboard,
+        path: '/dashboard'
+    },
+    {
+        name: 'A-Akun',
+        component: A_Akun,
+        path: '/a-akun'
+    },
+    {
+        name: 'A-Pegawai',
+        component: A_Pegawai,
+        path: '/a-pegawai'
+    },
+    {
+        name: 'A-Anggota',
+        component: A_Anggota,
+        path: '/a-anggota'
+    },
+    {
+        name: 'A-Simpanan',
+        component: A_Simpanan,
+        path: '/a-simpanan'
+    },
+    {
+        name: 'A-Pinjaman',
+        component: A_Pinjaman,
+        path: '/a-pinjaman'
+    },
+    {
+        name: 'Api',
+        component: Api,
+        path: '/api'
     }
 ]
 
