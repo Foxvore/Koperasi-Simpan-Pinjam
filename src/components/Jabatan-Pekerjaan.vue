297564e14d2a6ec96b20b54c0c6cf83fafe590d5
Foxvore <imjohani2005@gmail.com>
2022-09-27 10:06:33 +0700

    1st Commit

---
+++ src/components/Jabatan-Pekerjaan.vue
@@ -0,0 +1,181 @@
+<template>
+    <Sidebar />
+
+    <main class="main" id="main">
+        <div class="pagetitle">
+            <h1>Daftar Jabatan & Pekerjaan</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">Home</li>
+                    <li class="breadcrumb-item active">Jabatan & Pekerjaan</li>
+                </ol>
+            </nav>
+        </div>
+
+        <section>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="row info">
+
+                        <div class="col-xxl-3 col-md-6">
+                            <div class="card info-card">
+                                <div class="card-body">
+                                    <router-link to="/a-pegawai"><h5 class="card-title">Total Pegawai</h5></router-link>
+                                    <div class="d-flex align-items-center">
+                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="fa-solid fa-user-tie"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>0 Orang</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xxl-3 col-md-6">
+                            <div class="card info-card">
+                                <div class="card-body">
+                                    <router-link to="a-anggota"><h5 class="card-title">Total Anggota</h5></router-link>
+                                    <div class="d-flex align-items-center">
+                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="fa-solid fa-users"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>0 Orang</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import axios from 'axios'
+import Sidebar from './Sidebar.vue'
+export default {
+    name: "Jabatan-Pekerjaan",
+    components : {
+        Sidebar
+    },
+    async mounted() {
+        kuki = 0;
+        
+        if (this.$cookies.get("jwt")) {
+            var acc = await axios.get("http://localhost:8080/api/v1/userInfo", {withCredentials: true});
+            console.log(acc.data.data.role)
+            if(acc) { // Login        
+                if (acc.data.data.role === 2 || acc.data.data.role === 3 || acc.data.data.role === 4 ) { // Pimpinan, Staff & Member
+                    this.$router.push({name : 'Home'})
+                } 
+            } 
+        } else if (kuki === 0) { //Not Login
+            this.$router.push({name : 'Home'})
+        }
+    }
+};
+</script>
+
+<style scoped>
+/*===== Page Title =====*/
+.pagetitle {
+    margin: 10px 0;
+}
+
+.pagetitle h1 {
+    font-size: 24px;
+    margin-bottom: 0;
+    font-weight: 600;
+    color: #ff9900;
+}
+
+.breadcrumb-item.active {
+    color: #ff9900;
+}
+
+/*===== Main =====*/
+#error {
+    font-size: 12px;
+    color: red;
+}
+
+#main {
+    height: 100vh;
+    padding: 20px 30px;
+    transition: all 0.3s;
+    background: #F7F5F2;
+}
+
+@media (max-width: 1199px) {
+  #main {
+    padding: 20px;
+  }
+}
+
+@media (min-width: 1200px) {
+    #main,
+    #footer {
+        margin-left: 300px;
+    }
+}
+
+section {
+    padding: 20px 0;
+}
+
+a {
+    color: #212529;
+}
+
+/* Info Cards */
+.dashboard .info-card {
+    padding-bottom: 10px;
+    border: none;
+}
+
+.dashboard .info-card h6 {
+    font-size: 28px;
+    color: #444;
+    font-weight: 700;
+    margin: 0;
+    padding: 0;
+}
+
+.dashboard .card-icon {
+    font-size: 32px;
+    line-height: 0;
+    width: 64px;
+    height: 64px;
+    flex-shrink: 0;
+    flex-grow: 0;
+}
+
+.dashboard .card-icon {
+    color: #ff9900;
+    background: #ffecdf;
+}
+
+.info {
+    width: 1200px;
+}
+.card-title {
+    margin-bottom: 20px;
+}
+
+.card-title::before {
+    content: "";
+    position: absolute;
+    left: 15px;
+    top: 40px;
+    height: 4px;
+    width: 70px;
+    border-radius: 5px;
+    background: linear-gradient(135deg, #fad88d, #FFB037);
+}
+</style>
