4a95e89b5fbf871d980bc91c91c8e5339a97dccc
Foxvore <imjohani2005@gmail.com>
2022-09-27 10:06:01 +0700

    Add "vue-cookies" Library

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 import router from './router'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import VueCookies from 'vue-cookies';
 
 createApp(App)
     .use(router)
     .use(MotionPlugin)
     .use(VueSweetalert2)
+    .use(VueCookies)
     .mount('#app');
